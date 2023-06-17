2119902ff1690ffce3a41ef2013df92ffde83717
Ayanda <amakhanya23@gmail.com>
2023-06-17 12:06:16 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,20 +12,20 @@ for (j = 0; j < 100; j++)
 {
 if (i < j)
 {
-	putchar((i / 10) + 48);
-	putchar((i % 10) + 48);
-	putchar(' ');
-	putchar((j / 10) + 48);
-	putchar((j % 10) + 48);
+putchar((i / 10) + 48);
+putchar((i % 10) + 48);
+putchar(' ');
+putchar((j / 10) + 48);
+putchar((j % 10) + 48);
 if (i != 98 || j != 99)
 {
-	putchar(',');
-	putchar(' ');
+putchar(',');
+putchar(' ');
 }
 }
 }
 }
 }
-	putchar('\n');
-	return (0);
+putchar('\n');
+return (0);
 }
