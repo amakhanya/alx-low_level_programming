265a1969e4fb165d4d7b9af5c93f46732c25a068
Ayanda <amakhanya23@gmail.com>
2023-06-23 09:15:57 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers-print 0-9
+ * return: void
+*/
+
+void print_numbers(void)
+{
+	char C;
+	
+	for (C = '0'; C <= '9'; C++)
+	{
+		_putchar(C);
+	}
+		_putchar('\n');
+}	
